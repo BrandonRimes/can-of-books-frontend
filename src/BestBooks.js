92d3626949bd86678f8454571b717ffcd3b497c7
Brandon Rimes <77939170+BrandonRimes@users.noreply.github.com>
2021-08-30 23:26:14 -0400

    linking backend, carousel

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
+
+
+const BOOKSERVER = process.env.REACT_APP_SERVER;
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,21 +14,56 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch books for the logged in user  */
+  componentDidMount() {
+    let booksReq = `${BOOKSERVER}/books`;
+
+    // if (this.state.books) {
+    //   // booksReq += `?email=${this.state.title}`;
+    // }
+
+    try {
+      const response = axios.get(booksReq);
+      this.setState({ books: response.data });
+      console.log(this.state.books);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   render() {
-
-    /* TODO: render user's books in a Carousel */
+    // {this.state.books.length &&
+    //   const carousel = this.state.books.map((book, i) => {
+    //   let carouselItem = `
+    //     <Carousel.Item key=${i}>
+    //       <Carousel.Caption>
+    //         <h3>${book.title}</h3>
+    //         <p>${book.description}</p>
+    //       </Carousel.Caption>
+    //     </Carousel.Item>
+    //     `
+    //   return carouselItem;
+    // })}
+    
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {/* {this.state.books.length ? (
+          <Carousel fade>
+            {this.state.books.map((book, i) => {
+              return (
+                <Carousel.Item key={i}>
+                  <Carousel.Caption>
+                    <h3>{book.title}</h3>
+                    <p>{book.description}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>)
+            })}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
-        )}
+        )} */}
       </>
     )
   }
