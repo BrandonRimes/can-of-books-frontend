5992b719adf141af1c4f0cfd6d0d2546da5d92a3
Brandon Rimes <77939170+BrandonRimes@users.noreply.github.com>
2021-08-31 21:22:39 -0400

    added create book form and modal

--- src/CreateBook.js
+++ src/CreateBook.js
@@ -1,13 +1,71 @@
 import { Component } from "react";
-import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 class CreateBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    }
+  }
 
-  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const bookInfo = {
+      title: e.target.formTitle.value,
+      description: e.target.formDescription.value,
+      status: e.target.formStatus.checked,
+      email: e.target.formEmail.value,
+    }
+    this.handleModal();
+    this.props.onCreate(bookInfo);
+    
+  }
+
+  handleModal = () => {
+    if(this.state.showModal) {
+      this.setState({ showModal: false });
+    } else {
+      this.setState({ showModal: true });
+    }
+    
+  }
 
   render() {
-    return <h1>CREATE A BOOK!</h1>
+    return (
+      <>
+      <Button onClick={this.handleModal}><h1>Create A Book!</h1></Button>
+      <Modal show={this.state.showModal} onHide={this.handleModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create A Book</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="formTitle" >
+            <Form.Control type="text" placeholder="Book Title e.g. The Chronicles of Narnia" required/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formDescription">
+            <Form.Label>Book Description</Form.Label>
+            <Form.Control as="textarea" rows={2} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formStatus">
+            <Form.Check type="checkbox" label="Read" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formEmail">
+            <Form.Control type="email" placeholder="Email Address" required/>
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Modal>
+      </>  
+    );
   }
 }
 
